6a939c2654f14e29a348b55e640f0c0d7234fec5
Saul473 <118771655+Saul473@users.noreply.github.com>
2023-05-09 23:06:21 -0600

    setup app.js to link with app.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from '../src/components/Header/header'
+import './css/app.css'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='header'>
+      <Header></Header>
     </div>
+    <div className='navbar'>
+      <Nav></Nav>
+    </div>
+    <div className='footer'>
+      <Footer></Footer>
+    </div>
+    </>
   );
 }
 
---
+++ src/components/Header/header.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+
+function Header() {
+    return (
+        <div>
+            <h1>Saul Navarro</h1>
+        </div>
+    )
+}
+
+export default Header;
